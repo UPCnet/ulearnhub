d59e6793a3adf95d9dde00dbb28c7feb87e5fd09
Carles Bruguera <carlesba@gmail.com>
2015-04-22 19:11:06 +0200

    ASk for acls

--- ulearnhub/rest/api/services.py
+++ ulearnhub/rest/api/services.py
@@ -3,10 +3,10 @@ from ulearnhub.rest import endpoint
 from ulearnhub.rest import JSONResourceEntity
 
 from ulearnhub.models.services import SERVICES
+from ulearnhub.
 
 
-@view_config(route_name='api_domain_service', request_method='POST')
-@endpoint()
+@view_config(route_name='api_domain_service', request_method='POST', permissions=)
 def domain_service(domain, request):
     Service = SERVICES[request.matchdict['service']]
     service = Service(domain, request)
