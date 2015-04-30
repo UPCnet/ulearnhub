6930baf2e4ea53c257e1aa07f49117671b18a8b9
Carles Bruguera <carlesba@gmail.com>
2015-04-30 12:58:13 +0200

    Define logger at init

--- ulearnhub/__init__.py
+++ ulearnhub/__init__.py
@@ -11,6 +11,9 @@ from pyramid_beaker import session_factory_from_settings
 from pyramid_beaker import set_cache_regions_from_settings
 from pyramid_multiauth import MultiAuthenticationPolicy
 
+import logging
+logger = logging.getLogger('ulearnhub')
+
 from max import patches
 import json
 import os
