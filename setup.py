58d342d6fd1e7f07d5f446bdc4b1f6da4e4ac363
Carles Bruguera <carlesba@gmail.com>
2015-05-19 11:57:50 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 ]
 
 setup(name='ulearnhub',
-      version='1.0',
+      version='1.1.dev0',
       description='ulearnhub',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
