d5f40cb9e72cd411d6028048aea8a44290ea8ef0
Carles Bruguera <carlesba@gmail.com>
2015-12-03 16:26:39 +0100

    Preparing release 1.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 ]
 
 setup(name='ulearnhub',
-      version='1.4.dev0',
+      version='1.4',
       description='ulearnhub',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
