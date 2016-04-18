85796e1a5622ffe8448e23503eeb2c35c4acb134
Pilar Marinas <pilar.marinas@upcnet.es>
2016-04-18 16:07:50 +0200

    Preparing release 1.5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 ]
 
 setup(name='ulearnhub',
-      version='1.5.dev0',
+      version='1.5',
       description='ulearnhub',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
