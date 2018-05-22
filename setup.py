482b22b12738f7d76de49bf5c5fca55e852087d3
Pilar Marinas <pilar.marinas@upcnet.es>
2018-05-22 16:06:58 +0200

    Back to development: 1.11

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 ]
 
 setup(name='ulearnhub',
-      version='1.10',
+      version='1.11.dev0',
       description='ulearnhub',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
